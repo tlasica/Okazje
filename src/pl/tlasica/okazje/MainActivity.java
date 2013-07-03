81f74c0b7d7f063d7025f8780a9ccf07e9160152
Tomasz Łasica <tlasica@o2.pl>
2013-07-03 11:37:33 +0200

    Initial version. Working for two days

---
+++ src/pl/tlasica/okazje/MainActivity.java
@@ -0,0 +1,66 @@
+package pl.tlasica.okazje;
+
+import java.util.Calendar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.text.format.DateFormat;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private TextView	mCurrDateTextView;
+	private TextView	mOccasionTextView;
+	private Calendar	currDate;
+	private Occasions	occasionsDict;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main);
+        
+        mCurrDateTextView = (TextView) findViewById( R.id.textview_current_date);
+        mOccasionTextView = (TextView) findViewById( R.id.textview_occasion);
+        
+        occasionsDict = new Occasions();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		updateContent();
+	}
+
+	private void updateContent() {
+		updateCurrentDate();
+		updateOccasion();
+	}
+
+	private void updateCurrentDate() {
+		currDate = Calendar.getInstance();
+		String dateStr = DateFormat.getDateFormat(getApplicationContext()).format( currDate.getTime());
+		mCurrDateTextView.setText( dateStr );		
+	}
+
+	private void updateOccasion() {
+		String occ = occasionsDict.getRandomOccasion( currDate );
+		mOccasionTextView.setText( occ );		
+		
+	}
+
+	public void onClickOccasion(View view) {
+		updateOccasion();
+	}
+	
+}
---
+++ src/pl/tlasica/okazje/Occasions.java
@@ -0,0 +1,104 @@
+package pl.tlasica.okazje;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import android.util.Log;
+import android.util.SparseArray;
+
+public class Occasions {
+
+	private static final String TAG = "OCCASIONS";
+	private int 							currDayNum;		//MMDD (bo lata przestępne)
+	private SparseArray<List<String>>	 	occasions;
+	List<String>							currDayOccasions;
+	Random									random;
+	
+	public Occasions() {
+		occasions = new SparseArray< List<String> >();
+		random = new Random( Calendar.getInstance().getTimeInMillis() );
+		initData();
+	}
+	
+	public String getRandomOccasion(Calendar cal) {
+		int num = Occasions.dateToDayNum(cal);
+		Log.d(TAG, "get random for" + String.valueOf( num ));
+		List<String> chooseFrom = occasions.get( num );
+		if ( chooseFrom != null ) {
+			int pos = random.nextInt( chooseFrom.size() );
+			return chooseFrom.get( pos );
+		}
+		else {
+			return "Nie ma dobrej okazji... Może by tak nie pić?";
+		}
+	}
+	
+	private void initData() {
+		setDay(703);
+		add("Imieniny Heliodora");
+		add("Imieniny Tryfona");
+		add("Imieniny Tomasza");
+		add("Imieniny Anatola");
+		add("Imieniny Jacka i Józefa");
+		add("Dzień Niepodległości Białorusi");
+		add("W roku 1519 Karniszyn uzyskał prawa miejskie");
+		add("W 1921 w Lublinie odbył się ślub Bolesława Bieruta");
+		add("W 1961 urodził się 30-milionowy obywatel Warszawy");
+		add("W 1995 Marian Dziurowicz został prezesem PZPN");
+		add("W 1996 odbyła się premiera filmu \"Dzień Niepodległości\"");
+		add("W 1757 urodził się Jan Paweł Woronicz");
+		add("W 1883 urodził się Franz Kafka");
+		add("W 1928 urodził się jan Machulski");
+		setDay(704);
+		add("Imieniny Aggeusza i Anatazego");
+		add("Imieniny Hiacynta i Innocentego");
+		add("Imieniny Jacka i Jacentego");
+		add("Imieniny Malwiny");
+		add("Imieniny Andrzeja");
+		add("Imieniny Elżbiety");
+		add("Imieniny Piotra");
+		add("Dzień Wolności Rwandy");
+		add("Dzień Niepodległości Stanów Zjednoczonych Ameryki");
+		add("Dzień Przyjaźni Filipińsko-Amerykańskiej");
+		add("W 1364 Barczewo uzyskało prawa miejskie");
+		add("W 1633 zwycięstwo wojsk polskich w bitwie pod Sasowym Rogiem");
+		add("W 1945 założono klub sportowy Pogoń Lębork");
+		add("W 1054 w gwiazdozbiorze Byka wybuchła supernowa SN 1054");
+		add("W 1776 uchwalono Deklarację Niepodległości Stanów Zjednoczonych");
+		add("W 1865 ukazała się powieść \"Alicja W Krainie Czarów \"");
+		add("W 1950 rozpoczęło nadawanie Radio Wolna Europa");
+		add("W 1982 po remisie 0:0 z ZSSR reprezentacja Polski awansowała do półfinału mundialu w Hiszpanii");
+		add("W 2012 Iwane Merabiszwili został premierem Gruzji");
+		add("W 1917 urodził się Manolete, hiszpański matador");
+		add("W 1974 urodził się Denis Pankratow, rosyjski pływak");
+	}
+	
+	public static int dateToDayNum(Calendar cal) {		
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int month = cal.get(Calendar.MONTH);
+		return 100 * (month+1) + day;
+	}
+	
+	
+	private void setDay(int dateNum) {
+		currDayNum = dateNum;
+		currDayOccasions = occasions.get( currDayNum );
+		if (currDayOccasions==null) {
+			currDayOccasions = new ArrayList<String>();
+			occasions.put( currDayNum, currDayOccasions );
+			Log.i(TAG, "day set to " + String.valueOf( dateNum ));
+		}
+	}
+		
+	
+	private void add(String occasion) {
+		if ( currDayOccasions != null ) {
+			currDayOccasions.add( occasion );
+		} else {
+			Log.e(TAG, "date not set (currDateOccasions is null" );
+		}
+	}
+	
+}
